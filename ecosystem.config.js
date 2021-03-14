27bde0a382e6d0d88071afcb47624da13ff6576f
Luciano Tassara <lucianotassara@gmail.com>
2021-03-14 17:15:45 -0300

    modified properties

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-      name: 'yalc',
+      name: 'yalc-evida',
       script: './index.js',
   
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.all("/", function (req, res, next) {
 });
 
 const portYalcApi = process.env.YALC_API_PORT || 3007;
-const mongoDBUri = process.env.YALC_MONGO_URI;
+const mongoDBUri = process.env.YALC_EVIDA_MONGO_URI;
 const mongoHostName = process.env.YALC_MONGO_HOSTNAME;
 
 app.listen(portYalcApi, () => {
